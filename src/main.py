deeecbcab51da446395af2818e71dd38c893ce65
Scott Meyers <sdmeyers@gmail.com>
2025-04-17 11:38:46 -0400

    ``` Remove public directory and update main script
    - Deleted `public/index.html` and `public/styles.css`.
    - Updated `main.py` to delete the `public` directory and copy files
      from `static` to `public` using `copy_files_recursive`.
    ```

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 def main():
-    dumb_text1 = TextNode("This is anchor text", TextType.LINK, "http://mydogateit.com")
-    print(dumb_text1)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
 main()
