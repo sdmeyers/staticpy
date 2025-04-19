e0afde17aef88cd8346d29852a6c8a01ea12122c
Scott Meyers <sdmeyers@gmail.com>
2025-04-19 11:28:24 -0400

    ``` Add blog posts and recursive page generation
    - Added three new blog posts: Glorfindel, LOTR Majesty, and Tom Bombadil.
    - Implemented recursive page generation in gencontent.py.
    - Updated main.py to use recursive page generation.
    - Added static images for blog posts.
    ```

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,6 +1,16 @@
 import os
+from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
 
 def generate_page(from_path, template_path, dest_path):
     print(f" * {from_path} {template_path} -> {dest_path}")
@@ -13,7 +23,6 @@ def generate_page(from_path, template_path, dest_path):
     template_file.close()
 
     node = markdown_to_html_node(markdown_content)
-    print(f"NODE: {node}")
     html = node.to_html()
 
     title = extract_title(markdown_content)
@@ -25,6 +34,7 @@ def generate_page(from_path, template_path, dest_path):
         os.makedirs(dest_dir_path, exist_ok=True)
     to_file = open(dest_path, "w")
     to_file.write(template)
+
 
 def extract_title(md):
     lines = md.split("\n")
